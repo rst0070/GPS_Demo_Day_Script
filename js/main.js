e4d94170ee0167b80de2a2bb5595cdfb7e37d797
rst0070 <52444137+rst0070@users.noreply.github.com>
2022-06-05 22:14:40 +0900

    html에 직접입력방식으로 변경

--- js/main.js
+++ js/main.js
@@ -1,43 +1,8 @@
 $(async ()=>{
-    function lnToP(str){
-        str = str.replace('\n', "</p/><br/><p>");
-        return "<p>"+str+"</p>";
-    }
-
-    function getContentElement(title, presenter, scriptKr, scriptEng){
-        scriptKr = lnToP(scriptKr);
-        scriptEng = lnToP(scriptEng);
-
-        return ""+
-        "<div class=\"content\">"+
-            "<div class=\"bar\">"+
-                "<p class=\"script_title\">"+title+"</p>"+
-                "<p class=\"script_presenter\">"+presenter+"</p>"+
-            "</div>"+
-            "<div class=\"script_kr\">"+scriptKr+"</div>"+
-            "<div class=\"script_eng\">"+scriptEng+"</div>"+
-        "</div>";
-    }
-
-    async function getScriptData(){
-        return await $.ajax("/gps-demo-day-script/data/script_data.json");
-    }
-
-    function insertContents(scriptData){
-        let container = $("#contents");
-
-        for(let data of scriptData){
-            let elementStr = getContentElement(data.title, data.presenter, data.script_kr, data.script_eng);
-            container.append(elementStr);
-        }
-    }
 
     let toggleOption = {
         duration: 500
     };
-
-    let data = (await getScriptData()).data;
-    insertContents(data);
 
     $(".script_presenter").css(
         {
